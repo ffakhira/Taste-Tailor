92350f02e9382c48974b16dea253a1e1c1469e76
fakhira <fakhiraalysa03@gmail.com>
2025-05-27 06:32:56 +0800

    ng

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
